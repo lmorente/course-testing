efb426f178c111d9e73efad52ffe8f72b79c11ce
lmorente <lourdesmorente+hub@gmail.com>
2021-06-27 16:24:54 +0200

    add new course

---
+++ src/test/java/com/demo/courses/testing/TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.courses.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
