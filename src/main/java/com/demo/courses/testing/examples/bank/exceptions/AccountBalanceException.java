bb9ce657571204ab646aca2a81735ffbecda0347
lmorente <lourdesmorente+hub@gmail.com>
2021-07-22 10:32:06 +0200

    delete duplicate info

--- src/main/java/com/demo/courses/testing/examples/bank/exceptions/AccountBalanceException.java
+++
@@ -1,8 +0,0 @@
-package com.demo.courses.testing.examples.bank.exceptions;
-
-public class AccountBalanceException extends Exception{
-
-    public AccountBalanceException(String msg){
-        super(msg);
-    }
-}
--- src/main/java/com/demo/courses/testing/examples/classExam/services/ExamService.java
+++
@@ -1,8 +0,0 @@
-package com.demo.courses.testing.examples.classExam.services;
-
-import com.demo.courses.testing.examples.classExam.models.Exam;
-
-public interface ExamService {
-
-    Exam findExamByName(String name);
-}
